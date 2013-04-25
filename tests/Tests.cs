92ddb5194d1a1b4dac58a914081baf99ee6809ce
Darrell Mozingo <darrell@mozingo.net>
2013-04-25 17:45:57 +0100

    Added test

---
+++ tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System.Configuration;
+using System.Web;
+using NUnit.Framework;
+
+namespace tests
+{
+    public class Tests
+    {
+		[Test]
+		public void Access_app_setting_fails_after_calling_parse_query_string()
+		{
+			Assert.That(ConfigurationManager.AppSettings["SomeKey"], Is.Not.Null, "Null on first try");
+			Assert.That(HttpUtility.ParseQueryString("example.com?query=value"), Is.Not.Null);
+			Assert.That(ConfigurationManager.AppSettings["SomeKey"], Is.Not.Null, "Null on second try");
+		}
+    }
+}
